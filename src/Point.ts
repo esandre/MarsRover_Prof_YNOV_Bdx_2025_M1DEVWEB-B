e29b192682885d7e1e2c33cf92f7a528769150d9
Enzo Sandre <enzo@enzosandre.fr>
2025-02-11 10:37:14 +0100

    Enigme : départ

---
+++ src/Point.ts
@@ -0,0 +1,15 @@
+class Point {
+    private readonly _x: number;
+    private readonly _y: number;
+
+    public constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
+    }
+}
+
+class Program {
+    public static ManhattanDistance(a: Point, b: Point): number {
+
+    }
+}
