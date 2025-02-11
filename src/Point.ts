6c5b34522174cf6125b1d63b594dbecd2d9d1e2f
Enzo Sandre <enzo@enzosandre.fr>
2025-02-11 12:20:50 +0100

    Enigme : réponse

--- src/Point.ts
+++ src/Point.ts
@@ -6,10 +6,21 @@ class Point {
         this._x = x;
         this._y = y;
     }
+
+    public DistanceWith(other: Point): number{
+        const xDelta = Math.abs(this._x - other._x);
+        const yDelta = Math.abs(this._y - other._y);
+        return xDelta + yDelta;
+    }
 }
 
 class Program {
     public static ManhattanDistance(a: Point, b: Point): number {
-
+        return a.DistanceWith(b);
     }
 }
+
+const p1 = new Point(16, 78);
+const p2 = new Point(9, 37);
+
+console.log(Program.ManhattanDistance(p1, p2));
